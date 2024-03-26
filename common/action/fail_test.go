cd87f60612a1c1befee6833a8d5920e923d674c2
Joshua Chaitin-Pollak <josh@c11k.org>
2024-03-26 14:51:05 +0000

    return a concrete type, write a unit test

---
+++ common/action/fail_test.go
@@ -0,0 +1,41 @@
+package action
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jbcpollak/greenstalk"
+	"github.com/rs/zerolog/log"
+
+	"github.com/jbcpollak/greenstalk/common/composite"
+	"github.com/jbcpollak/greenstalk/core"
+	"github.com/jbcpollak/greenstalk/util"
+)
+
+type EmptyBlackboard struct{}
+
+func TestFail(t *testing.T) {
+	// Synchronous, so does not need to be cancelled.
+	ctx := context.Background()
+
+	fail := Fail[EmptyBlackboard](FailParams{}, struct{}{})
+
+	var failSequence = composite.Sequence[EmptyBlackboard](
+		fail,
+	)
+
+	tree, err := greenstalk.NewBehaviorTree(ctx, failSequence, EmptyBlackboard{})
+	if err != nil {
+		panic(err)
+	}
+
+	evt := core.DefaultEvent{}
+	status := tree.Update(evt)
+	util.PrintTreeInColor(tree.Root)
+	if status != core.StatusFailure {
+		t.Errorf("Unexpectedly got %v", status)
+
+	}
+
+	log.Info().Msg("Done!")
+}
