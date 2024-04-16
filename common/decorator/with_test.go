f6dd7fbf77bc8c9bd54dc73f02a413deb2d8224b
Nik Ivanov <nivanov@ati-inc.co>
2024-04-15 20:56:08 -0400

    added test for with

---
+++ common/decorator/with_test.go
@@ -0,0 +1,110 @@
+package decorator
+
+import (
+	"context"
+	"io"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/jbcpollak/greenstalk"
+	"github.com/jbcpollak/greenstalk/common/action"
+	"github.com/jbcpollak/greenstalk/common/composite"
+	"github.com/jbcpollak/greenstalk/core"
+	"github.com/jbcpollak/greenstalk/util"
+	"github.com/rs/zerolog/log"
+)
+
+type testCloser struct {
+	closeCalled *bool
+}
+
+func (t testCloser) Close() error {
+	*t.closeCalled = true
+	return nil
+}
+
+func TestWith(t *testing.T) {
+	var wg sync.WaitGroup
+
+	// Synchronous, so does not need to be cancelled.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigChan := make(chan bool)
+
+	childCalled := new(bool)
+	*childCalled = false
+	child := action.FunctionAction[core.EmptyBlackboard](action.FunctionActionParams{
+		Func: func() core.NodeResult {
+			*childCalled = true
+			return core.StatusSuccess
+		},
+	})
+
+	closeCalled := new(bool)
+	*closeCalled = false
+	closer := testCloser{
+		closeCalled: closeCalled,
+	}
+	with := With(child, func() (io.Closer, error) {
+		return closer, nil
+	})
+	// TODO: testCloser and the whole closeCalled thing above just creates a struct with an attached method
+	// Is there a more succinct way to write this?
+
+	params := action.SignallerParams[bool]{
+		BaseParams: "Signaller",
+
+		Channel: sigChan,
+		Signal:  true,
+	}
+	signaller := action.Signaller[core.EmptyBlackboard](params)
+
+	var testSequence = composite.Sequence(
+		with,
+		signaller,
+	)
+
+	tree, err := greenstalk.NewBehaviorTree(ctx, testSequence, core.EmptyBlackboard{})
+	if err != nil {
+		panic(err)
+	}
+
+	evt := core.DefaultEvent{}
+	wg.Add(1)
+	go func() {
+		tree.EventLoop(evt)
+		wg.Done()
+	}()
+
+	util.PrintTreeInColor(tree.Root)
+
+	d := time.Duration(100) * time.Millisecond
+
+LOOP:
+	for {
+		select {
+		case c := <-sigChan:
+			log.Info().Msgf("loop is finished %v", c)
+
+			break LOOP
+		case <-time.After(d):
+			t.Errorf("Timeout after delaying %v", d)
+		}
+	}
+
+	cancel()
+	wg.Wait()
+	status := tree.Root.Status()
+	if status != core.StatusSuccess {
+		t.Errorf("Unexpectedly got %v", status)
+	}
+
+	if !*childCalled {
+		t.Errorf("Child was not called")
+	}
+
+	if !*closeCalled {
+		t.Errorf("Close was not called")
+	}
+}
