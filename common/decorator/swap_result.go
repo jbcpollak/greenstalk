6459f518e04c061a0b4f66865e909cfb61eb6ddd
Nik Ivanov <nivanov@ati-inc.co>
2024-06-14 13:16:39 -0400

    feat: add SwapResult

---
+++ common/decorator/swap_result.go
@@ -0,0 +1,47 @@
+package decorator
+
+import (
+	"context"
+
+	"github.com/jbcpollak/greenstalk/core"
+)
+
+func SwapResult[Blackboard any](child core.Node[Blackboard], swapFrom core.Status, swapTo core.Status) core.Node[Blackboard] {
+	if swapFrom != core.StatusSuccess && swapFrom != core.StatusFailure {
+		panic("cannot swap from statuses other than Success or Failure")
+	}
+
+	if swapTo != core.StatusSuccess && swapTo != core.StatusFailure {
+		panic("cannot swap to statuses other than Success or Failure")
+	}
+
+	base := core.NewDecorator(core.BaseParams("SwapResult"), child)
+	return &swapResult[Blackboard]{base, swapFrom, swapTo}
+}
+
+type swapResult[Blackboard any] struct {
+	core.Decorator[Blackboard, core.BaseParams]
+	swapFrom core.Status
+	swapTo   core.Status
+}
+
+func (d *swapResult[Blackboard]) Activate(ctx context.Context, bb Blackboard, evt core.Event) core.ResultDetails {
+	return d.Tick(ctx, bb, evt)
+}
+
+// Tick ...
+func (d *swapResult[Blackboard]) Tick(ctx context.Context, bb Blackboard, evt core.Event) core.ResultDetails {
+	result := core.Update(ctx, d.Child, bb, evt)
+	if result.Status() == d.swapFrom {
+		if d.swapTo == core.StatusSuccess {
+			return core.SuccessResult()
+		}
+		return core.FailureResult()
+	}
+	return result
+}
+
+// Leave ...
+func (d *swapResult[Blackboard]) Leave(bb Blackboard) error {
+	return nil
+}
