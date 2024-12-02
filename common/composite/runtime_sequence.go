3ecdcdf5cc23b632d9a373adb676b3c9a25a3037
Nik Ivanov <nivanov@ati-inc.co>
2024-12-02 15:38:02 -0500

    feat: runtime sequence

---
+++ common/composite/runtime_sequence.go
@@ -0,0 +1,40 @@
+package composite
+
+import (
+	"context"
+
+	"github.com/jbcpollak/greenstalk/core"
+)
+
+func RuntimeSequence[Blackboard any](childrenFn func() []core.Node[Blackboard]) core.Node[Blackboard] {
+	base := core.NewRuntimeComposite(core.BaseParams("RuntimeSequence"), childrenFn)
+	return &runtimeSequence[Blackboard]{RuntimeComposite: base}
+}
+
+type runtimeSequence[Blackboard any] struct {
+	core.RuntimeComposite[Blackboard, core.BaseParams]
+}
+
+func (s *runtimeSequence[Blackboard]) Activate(ctx context.Context, bb Blackboard, evt core.Event) core.ResultDetails {
+	s.Children = s.ChildrenFn()
+	s.CurrentChild = 0
+
+	// Tick as expected
+	return s.Tick(ctx, bb, evt)
+}
+
+func (s *runtimeSequence[Blackboard]) Tick(ctx context.Context, bb Blackboard, evt core.Event) core.ResultDetails {
+	for s.CurrentChild < len(s.Children) {
+		result := core.Update(ctx, s.Children[s.CurrentChild], bb, evt)
+		if result.Status() != core.StatusSuccess {
+			return result
+		}
+		s.CurrentChild++
+	}
+	return core.SuccessResult()
+}
+
+func (s *runtimeSequence[Blackboard]) Leave(bb Blackboard) error {
+	s.Children = []core.Node[Blackboard]{}
+	return nil
+}
