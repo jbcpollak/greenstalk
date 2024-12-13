0185ba1482457938030eea32007ff9bbb14f8cae
Nik Ivanov <nivanov@ati-inc.co>
2024-12-13 16:05:53 -0500

    dynamic decorator is in, runtime sequence is out

---
+++ core/dynamic_decorator.go
@@ -0,0 +1,27 @@
+package core
+
+import "fmt"
+
+type DynamicDecorator[Blackboard any, P Params] struct {
+	BaseNode[P]
+	Child   Node[Blackboard]
+	ChildFn func() (Node[Blackboard], error)
+}
+
+func NewDynamicDecorator[Blackboard any, P Params](params P, childFn func() (Node[Blackboard], error)) DynamicDecorator[Blackboard, P] {
+	return DynamicDecorator[Blackboard, P]{
+		BaseNode: newBaseNode(CategoryDecorator, params),
+		ChildFn:  childFn,
+	}
+}
+
+func (c *DynamicDecorator[Blackboard, P]) Walk(walkFn WalkFunc[Blackboard], level int) {
+	walkFn(c, level)
+	if c.Child != nil {
+		c.Child.Walk(walkFn, level+1)
+	}
+}
+
+func (d *DynamicDecorator[Blackboard, P]) String() string {
+	return fmt.Sprintf("*d %s (%v)", d.Params.Name(), d.Params)
+}
