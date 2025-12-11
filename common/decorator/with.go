d72193c6cf790971d96bdac0ed1996ccf6ef899e
Matthew Gabeler-Lee <mgabeler-lee@ati-inc.co>
2025-12-11 15:33:45 -0500

    fix: nicer whitespace

--- common/decorator/with.go
+++ common/decorator/with.go
@@ -6,12 +6,19 @@ import (
 	"github.com/jbcpollak/greenstalk/v2/core"
 )
 
-func WithNamed(name string, createCloseable func(context.Context) (closeFn func(context.Context) error, err error), child core.Node) core.Node {
+func WithNamed(
+	name string,
+	createCloseable func(context.Context) (closeFn func(context.Context) error, err error),
+	child core.Node,
+) core.Node {
 	base := core.NewDecorator(core.BaseParams(name), child)
 	return &with{Decorator: base, createCloseable: createCloseable}
 }
 
-func With(createCloseable func(context.Context) (closeFn func(context.Context) error, err error), child core.Node) core.Node {
+func With(
+	createCloseable func(context.Context) (closeFn func(context.Context) error, err error),
+	child core.Node,
+) core.Node {
 	return WithNamed("With", createCloseable, child)
 }
 
@@ -31,12 +38,10 @@ func (d *with) Activate(ctx context.Context, evt core.Event) core.ResultDetails 
 	return d.Tick(ctx, evt)
 }
 
-// Tick ...
 func (d *with) Tick(ctx context.Context, evt core.Event) core.ResultDetails {
 	return core.Update(ctx, d.Child, evt)
 }
 
-// Leave ...
 func (d *with) Leave(ctx context.Context) error {
 	return d.closeFn(ctx)
 }
