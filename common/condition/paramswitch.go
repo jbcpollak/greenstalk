e61f6d52d0335c6836c74ebe56cc2e0f1f34fc4a
Chris Urffer <c.urffer@live.com>
2026-03-06 17:57:28 -0500

    renamed paramSwitch to switchMap

--- common/condition/paramswitch.go
+++ common/condition/paramswitch.go
@@ -9,28 +9,28 @@ import (
 	"github.com/jbcpollak/greenstalk/v2/core"
 )
 
-type ParamSwitchFunc[T comparable] func() T
+type SwitchFunc[T comparable] func() T
 
-// ParamSwitch activates the child at the map key returned by the switch function.
-// Note that an If node is a rudimentary form of a ParamSwitch node with two children
+// SwitchMap activates the child at the map key returned by the switch function.
+// Note that an If node is a rudimentary form of a SwitchMap node with two children
 // and the function returning 0 / 1 for true / false.
-func ParamSwitchNamed[T comparable](name string, switchFunc ParamSwitchFunc[T], children map[T]core.Node) core.Node {
+func SwitchMapNamed[T comparable](name string, switchFunc SwitchFunc[T], children map[T]core.Node) core.Node {
 	base := core.NewComposite(core.BaseParams(name), slices.Collect(maps.Values(children)))
-	return &paramSwitchNode[T]{Composite: base, switchFunc: switchFunc, children: children}
+	return &switchMapNode[T]{Composite: base, switchFunc: switchFunc, children: children}
 }
 
-func ParamSwitch[T comparable](switchFunc ParamSwitchFunc[T], children map[T]core.Node) core.Node {
-	return ParamSwitchNamed("ParamSwitch", switchFunc, children)
+func SwitchMap[T comparable](switchFunc SwitchFunc[T], children map[T]core.Node) core.Node {
+	return SwitchMapNamed("SwitchMap", switchFunc, children)
 }
 
-type paramSwitchNode[T comparable] struct {
+type switchMapNode[T comparable] struct {
 	core.Composite[core.BaseParams]
-	switchFunc      ParamSwitchFunc[T]
+	switchFunc      SwitchFunc[T]
 	children        map[T]core.Node
 	currentChildKey T
 }
 
-func (i *paramSwitchNode[T]) Activate(ctx context.Context, evt core.Event) core.ResultDetails {
+func (i *switchMapNode[T]) Activate(ctx context.Context, evt core.Event) core.ResultDetails {
 	switchKey := i.switchFunc()
 	if _, ok := i.children[switchKey]; !ok {
 		return core.ErrorResult(fmt.Errorf("Switch key does not exist: %v", switchKey))
@@ -41,13 +41,13 @@ func (i *paramSwitchNode[T]) Activate(ctx context.Context, evt core.Event) core.
 	return i.Tick(ctx, evt)
 }
 
-func (s *paramSwitchNode[T]) Tick(ctx context.Context, evt core.Event) core.ResultDetails {
+func (s *switchMapNode[T]) Tick(ctx context.Context, evt core.Event) core.ResultDetails {
 	child := s.children[s.currentChildKey]
 	return core.Update(ctx, child, evt)
 }
 
-func (s *paramSwitchNode[T]) Leave(context.Context) error {
+func (s *switchMapNode[T]) Leave(context.Context) error {
 	return nil
 }
 
-var _ core.Node = (*paramSwitchNode[any])(nil)
+var _ core.Node = (*switchMapNode[any])(nil)
--- common/condition/switch.go
+++ common/condition/switch.go
@@ -7,19 +7,19 @@ import (
 // Switch activates the child at the index returned by the switch function.
 // Note that an If node is a rudimentary form of a Switch node with two children
 // and the function returning 0 / 1 for true / false.
-func SwitchNamed(name string, switchFunc ParamSwitchFunc[int], children ...core.Node) core.Node {
+func SwitchNamed(name string, switchFunc SwitchFunc[int], children ...core.Node) core.Node {
 	base := core.NewComposite(core.BaseParams(name), children)
 	childrenMap := map[int]core.Node{}
 	for i, child := range children {
 		childrenMap[i] = child
 	}
-	return &paramSwitchNode[int]{
+	return &switchMapNode[int]{
 		Composite:  base,
 		switchFunc: switchFunc,
 		children:   childrenMap,
 	}
 }
 
-func Switch(switchFunc ParamSwitchFunc[int], children ...core.Node) core.Node {
+func Switch(switchFunc SwitchFunc[int], children ...core.Node) core.Node {
 	return SwitchNamed("Switch", switchFunc, children...)
 }
